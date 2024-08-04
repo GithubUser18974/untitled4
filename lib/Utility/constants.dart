4431c496d8292d67ead060c7aeff7150e539bac5
Github User <mohamedaraby1296@gmail.com>
2024-08-04 16:28:23 +0300

    before finish the chats

---
+++ lib/Utility/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+TextStyle KTopCategoryAppButtonStyle = TextStyle(
+  color: Colors.white,
+  fontSize: 25,
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,303 @@
 import 'package:flutter/material.dart';
 
+import 'Utility/constants.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'WhatApp Design',
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xFF171717),
+        body: Stack(
+          children: [
+            Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(top: 40, left: 5, right: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(Icons.menu),
+                        iconSize: 32,
+                        color: Colors.white,
+                      ),
+                      IconButton(
+                          iconSize: 32,
+                          color: Colors.white,
+                          onPressed: () {},
+                          icon: Icon(Icons.search))
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.only(left: 10),
+                    children: [
+                      TextButton(
+                        onPressed: () {},
+                        child:
+                            Text("Messages", style: KTopCategoryAppButtonStyle),
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child:
+                            Text('Online', style: KTopCategoryAppButtonStyle),
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child:
+                            Text('Groups', style: KTopCategoryAppButtonStyle),
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Channels',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          'More',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 35,
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Positioned(
+              top: 190,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 220,
+                decoration: BoxDecoration(
+                  color: Color(0xFF27C1a9),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(
+                      (40),
+                    ),
+                    topRight: Radius.circular(
+                      (40),
+                    ),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(top: 15, left: 25, right: 25),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Favourite Contacts',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 13,
+                            ),
+                          ),
+                          IconButton(
+                            iconSize: 32,
+                            color: Colors.white,
+                            onPressed: () {},
+                            icon: Icon(Icons.more_horiz),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 90,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                          circle_colum_avatar(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              top: 365,
+              right: 0,
+              left: 0,
+              bottom: 0,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 15),
+                decoration: BoxDecoration(
+                  color: Color(0xFFEFFFFC),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(
+                      (40),
+                    ),
+                  ),
+                ),
+                child: ListView(
+                  padding: EdgeInsets.only(left: 25),
+                  children: [
+                    Row(children: [
+                      Row(
+                        children: [
+                          user_avatar(),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                ('Lora'),
+                                style: TextStyle(color: Colors.black),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                ('Hello, Hope all is well with you'),
+                                style: TextStyle(color: Colors.black),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            '20:22',
+                            style: TextStyle(fontSize: 10),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          CircleAvatar(
+                            radius: 10,
+                            backgroundColor: Colors.tealAccent,
+                            child: Text(
+                              '5',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 10,
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ]),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class circle_colum_avatar extends StatelessWidget {
+  const circle_colum_avatar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 7.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          user_avatar(),
+          SizedBox(
+            height: 5,
+          ),
+          Text(
+            'Alla',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 15,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class user_avatar extends StatelessWidget {
+  const user_avatar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 32,
+      backgroundColor: Colors.lightBlueAccent,
+      child: CircleAvatar(
+        radius: 29,
+        backgroundImage: Image.asset('assets/images/img1.jpeg').image,
+      ),
     );
   }
 }
